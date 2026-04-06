1bc0353b60a4fa5d8e0a2ed889d34683c00f57b9
walidkm <walidmitawa@gmail.com>
2026-04-06 23:38:52 +0300

    dev 7
    Signed-off-by: walidkm <walidmitawa@gmail.com>

--- apps/admin/src/components/ContentEditor.tsx
+++ apps/admin/src/components/ContentEditor.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { type Selection } from "./ContentSidebar";
 import { SubjectForm } from "./SubjectForm";
 import { TopicForm } from "./TopicForm";
